be14c844e9309a677eba65f2c736329fcdb89af7
Jiří Kofránek <kofranek@gmail.com>
2021-08-28 15:43:55 +0200

    Exercise 3

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,16 +1,28 @@
 import { DonateForm } from './donate-form'
+import {DonateList} from './donate-list'
+
+const mockDonates = [
+  { amount: 4, date: new Date() },
+  { amount: 20, date: new Date() },
+  { amount: 3, date: new Date() },
+  { amount: 1, date: new Date() }
+]
+
 
 export default class APP {
 
  #donateForm
+ #donateList
   constructor () {
     this.#donateForm = new DonateForm()
+    this.#donateList = new DonateList(mockDonates)
   }
 
-  run () {
-    
+
+  run () { 
     const donateHTML = this.#donateForm.render()
-    console.log('donateForm=', this.#donateForm)
-    document.body.append(donateHTML)
+    const donateList = this.#donateList.render()
+    //console.log('donateForm=', this.#donateForm)
+    document.body.append(donateHTML,donateList)
   }
 }
---
+++ src/modules/donate-list.js
@@ -0,0 +1,43 @@
+export class DonateList {
+  #donatesContainer
+  #donates
+  constructor (donates) {
+    this.#donatesContainer = document.createElement('div')
+    this.#donatesContainer.className = 'donates-container'
+    this.#donates = donates
+  }
+
+  render () {
+    const h2 = document.createElement('h2')
+    h2.className = 'donates-container__title'
+    h2.textContent = 'Список донатов'
+    const donatesContainerDonates = this.createDonatesList(this.#donates)
+    this.#donatesContainer.append(h2, donatesContainerDonates)
+    console.log('#donatesContainer=', this.#donatesContainer)
+    return this.#donatesContainer
+  }
+
+  dateString (date) {
+    const dateText = `${date.getDate()}. ${date.getMonth() +
+      1}. ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    return dateText
+  }
+
+  createDonatesList (donates) {
+    const donatesContainerDonates = document.createElement('div')
+    donatesContainerDonates.className = 'donates-container__donates'
+    donates.forEach(el => {
+      const amount = el.amount
+      const date = el.date
+      const donateItem = document.createElement('div')
+      donateItem.className = 'donate-item'
+      const dateString = this.dateString(date)
+      const b = document.createElement('b')
+      b.textContent = ` - ${amount}$`
+      donateItem.append(dateString,b)
+
+      donatesContainerDonates.append(donateItem)
+    })
+    return donatesContainerDonates
+  }
+}
